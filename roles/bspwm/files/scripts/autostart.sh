b6f231eb948d40a76fc27681d8e26ac35b727aa9
pawndev <coquelet.c@gmail.com>
2022-05-22 14:56:34 +0200

    feat: floating 1password, rofi shortcuts, touchpad config

--- roles/bspwm/files/scripts/autostart.sh
+++ roles/bspwm/files/scripts/autostart.sh
@@ -11,3 +11,11 @@ function run {
 run feh --bg-fill ~/Pictures/Wallpapers/ffxiv-endwalker.png
 run sxhkd
 run 1password --silent
+run nm-applet
+setxkbmap us -option compose:ralt
+
+# touchpad config
+## two finger click context menu
+xinput set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Click Method Enabled" 0 1
+## enable "reverse scrolling"
+xinput set-prop "DLL0945:00 04F3:311C Touchpad" "libinput Natural Scrolling Enabled" 0
