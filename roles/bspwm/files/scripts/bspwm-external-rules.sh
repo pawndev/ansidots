baabdcd031b33214d7ccd4afb6b9f692b3ac5a75
pawndev <coquelet.c@gmail.com>
2022-05-22 21:04:31 +0200

    feat: Add floating rule for alacrittyvpn

---
+++ roles/bspwm/files/scripts/bspwm-external-rules.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+wid=$1
+class=$2
+instance=$3
+consequences=$4
+name=$(xwininfo -id "$wid" | sed ' /^xwininfo/!d ; s,.*"\(.*\)".*,\1,')
+
+rules=()
+
+SCREEN_WIDTH=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1 | head -n 1)
+SCREEN_HEIGHT=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2 | head -n 1)
+WIDTH=$(echo $(( (${SCREEN_WIDTH}/4)*3)) | cut -f1 -d".")
+HEIGHT=$(echo $(( (${SCREEN_HEIGHT}/4)*3)) | cut -f1 -d".")
+
+main() {
+    echo "" > /tmp/log/boulou.txt
+    echo "WID: $wid" >> /tmp/log/boulou.txt
+    echo "class: $class" >> /tmp/log/boulou.txt
+    echo "instance: $instance" >> /tmp/log/boulou.txt
+    echo "name: $name" >> /tmp/log/boulou.txt
+    case "$class" in
+        [Pp]hototonic)
+            rules+=( "state=floating center=on rectangle=${WIDTH}x${HEIGHT}+0+0" )
+            ;;
+        Alacritty)
+            [[ $instance == "alacrittyvpn" ]] && rules+=( "state=floating center=on rectangle=${WIDTH}x${HEIGHT}+0+0" )
+            ;;
+        TelegramDesktop)
+            # Always show the Media viewer on the focused desktop
+            # Useful if Telegram is not on its default desktop
+            [[ $name == "Media viewer" ]] && rules+=( "desktop=focused" )
+            ;;
+        "")
+            sleep 0.5
+
+            wm_class=($(xprop -id $wid | grep "WM_CLASS" | grep -Po '"\K[^,"]+'))
+
+            class=${wm_class[-1]}
+
+            [[ ${#wm_class[@]} == "2" ]] && instance=${wm_class[0]}
+
+            [[ -n "$class" ]] && main
+            ;;
+    esac
+    case "$name" in
+        "Event Tester") # xev
+            rules+=( "state = floating" )
+            ;;
+    esac
+    echo "rules: $rules" >>/tmp/log/boulou.txt
+    echo "$rules"
+}
+
+main
