52deff5b9eba705915c419901f24dad251f73d60
pawndev <coquelet.c@gmail.com>
2022-05-28 17:49:45 +0200

    feat(polybar): Add current music play

---
+++ roles/userscripts/files/polymusic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+players=("mopidy" "spotify")
+players_icon=()
+
+get_player_status() {
+    playerctl -p $1 status
+}
+
+format_current_song() {
+    playerctl -p $1 metadata --format "{{ artist }} - {{ title }}"
+}
+
+main() {
+    has_break=1
+    for t in ${players[@]}; do
+        status=$(get_player_status $t)
+        if [[ "$status" == "Playing" ]]; then
+            echo $(format_current_song $t)
+            has_break = 0
+            break;
+        fi
+    done
+    if [[ $has_break == 1 ]]; then
+        echo ""
+    fi
+}
+
+main
