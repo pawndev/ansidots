1887cf4a61db347998473a26592d6df9f4196ac0
pawndev <coquelet.c@gmail.com>
2022-05-28 14:22:10 +0200

    fix(userscript/volume.sh): Use the correct default output device

--- roles/userscripts/files/volume.sh
+++ roles/userscripts/files/volume.sh
@@ -9,9 +9,11 @@ amixer -q -D pulse sset Master "$@" > /dev/null
 # amixer -q -D pulse sset Master 2%{-,+} unmute
 
 # Query amixer for the current volume and whether or not the speaker is muted
-volume="$(amixer -c 0 get Master | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//g')"
-mute="$(amixer -c 0 get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
-if [[ $volume == 0 || "$mute" == "off" ]]; then
+# volume="$(amixer -c 0 get Master | tail -1 | awk '{print $4}' | sed 's/[^0-9]*//g')"
+# mute="$(amixer -c 0 get Master | tail -1 | awk '{print $6}' | sed 's/[^a-z]*//g')"
+volume="$(amixer -D pulse get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)"
+mute="$(amixer -D pulse get Master | grep '%' | grep -oE '[^ ]+$' | grep off | sed 's/[^a-z]*//g' | tail -1)"
+if [[ $volume == 0 || $mute == "off" ]]; then
     # Show the sound muted notification
     dunstify -a "changeVolume" -u low -i audio-volume-muted -h string:x-dunst-stack-tag:$msgTag "Volume muted" 
 else
