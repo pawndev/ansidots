3c3720c4f4414a399e888da2412ec6b6c88c7eca
pawndev <coquelet.c@gmail.com>
2022-05-28 17:49:12 +0200

    feat(bspwm/autostart): Blueman and three finger touchpad click

--- roles/bspwm/files/scripts/autostart.sh
+++ roles/bspwm/files/scripts/autostart.sh
@@ -12,6 +12,7 @@ run feh --bg-fill ~/Pictures/Wallpapers/ffxiv-endwalker.png
 run sxhkd
 run 1password --silent
 run nm-applet
+run blueman-applet
 setxkbmap us -option compose:ralt
 
 # touchpad config
@@ -19,3 +20,5 @@ setxkbmap us -option compose:ralt
 xinput set-prop "DLL0945:00 04F3:311C Touchpad" "libinput Click Method Enabled" 0 1
 ## enable "reverse scrolling"
 xinput set-prop "DLL0945:00 04F3:311C Touchpad" "libinput Natural Scrolling Enabled" 0
+## Enable middle click with 3 fingers click
+xinput set-prop "DLL0945:00 04F3:311C Touchpad" "libinput Scroll Method Enabled" 0 1
